8dbda8e283026b5dde1f74cca222fc96d9737799
Jarosław Bagnicki <jarek@bagnicki.net>
2021-06-15 16:57:46 +0200

    Update Deploy-CFTemplate script

--- 03/ps-scripts/Deploy-CFTemplate.ps1
+++ 03/ps-scripts/Deploy-CFTemplate.ps1
@@ -5,21 +5,37 @@ param(
     [string] $Component,
     [string] $Stack,
     [string] $Template,
-    [string] $Params
+    [string] $Parameters
 )
 
-$TemplatePath = "$Project/$Component/templates/$Template.yaml"
-$ParamsPath = "$Project/$Component/templates/$Params-$Stage.json"
-$StackName = "$Project-$Component-$Stack-$Stage"
+$templatePath = Resolve-Path "$Project/$Component/templates/$Template.yaml"
+if (!(Test-Path $templatePath)) {
+    Throw "Template file not exist!"
+}
 
-$ParsedParams = ConvertFrom-Json $ParamsPath
+$parametersPath = Resolve-Path "$Project/$Component/parameters/$Parameters-$Stage.json"
+if (!(Test-Path $parametersPath)) {
+    Throw "Parameters file not exist!"
+}
 
-$Result = aws cloudformation deploy `
-    --template-file $TemplatePath`
-    --stack-name $StackName `
+$stackName = "$Project-$Component-$Stack-$Stage"
+
+$parameterOverridesArray = Get-Content $parametersPath | ConvertFrom-Json
+$parameterOverridesHastable = @{}
+$parameterOverridesArray | ForEach-Object { $parameterOverridesHastable[$_.ParameterKey] = $_.ParameterValue }
+
+$temp = @{
+    Project = 'website'
+    Stage = 'dev'
+    Component = 'single-tempalate'
+}
+
+$result = aws cloudformation deploy `
+    --template-file $templatePath.Path`
+    --stack-name $stackName `
     --no-fail-on-empty-changeset `
-    --parameter-overrides $ParsedParams `
+    --parameter-overrides file://$parametersPath `
     --region $Region `
     --tags Project=$Project Stage=$Stage Component=$Component
 
-Write-Output $Result
+Write-Output $result
