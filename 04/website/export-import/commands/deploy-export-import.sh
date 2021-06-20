d4e94aaccb8349163cb3d7a5d562ed38a2145b7f
Jarosław Bagnicki <jarek@bagnicki.net>
2021-06-20 07:29:16 +0200

    Deploy export-import stack set

--- 04/website/export-import/commands/deploy-export-import.sh
+++ 04/website/export-import/commands/deploy-export-import.sh
@@ -2,7 +2,7 @@
 
 PROJECT="website"
 STAGE="dev"
-REGION="eu-west-1"
+REGION="eu-central-1"
 
 
 #########  log bucket ###########
@@ -24,14 +24,14 @@ PARAMETERS="website"
 TEMPLATE_FILE="$PROJECT/$COMPONENT/templates/$TEMPLATE.yaml"
 PARAM_FILE="$PROJECT/$COMPONENT/parameters/$PARAMETERS-$STAGE.json"
 
-PARAMS=$(cat $PARAM_FILE | jq -jr 'map("\(.ParameterKey)=\(.ParameterValue)") | join (" ")')
+# PARAMS=$(cat $PARAM_FILE | jq -jr 'map("\(.ParameterKey)=\(.ParameterValue)") | join (" ")')
 
 deploy="aws cloudformation deploy \
     --template-file $TEMPLATE_FILE \
     --stack-name $PROJECT-$COMPONENT-$STACK-$STAGE \
     --capabilities CAPABILITY_NAMED_IAM \
     --no-fail-on-empty-changeset \
-    --parameter-overrides $PARAMS \
+    --parameter-overrides file://$PARAM_FILE \
     --region $REGION \
     --tags Project=$PROJECT Stage=$STAGE Component=$COMPONENT"
 
