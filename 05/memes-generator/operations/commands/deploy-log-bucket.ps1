c2f476c0dda567291eab1dd735c9c7873be591b9
Jarosław Bagnicki <jarek@bagnicki.net>
2021-07-01 19:01:15 +0200

    Add delpoy-log-bucket ps script

--- 05/memes-generator/operations/commands/deploy-log-bucket.ps1
+++ 05/memes-generator/operations/commands/deploy-log-bucket.ps1
@@ -9,8 +9,31 @@ $component = "operations"
 
 $stack = "log-bucket"
 
+$stackName = "$project-$component-$stack-$stage"
+
 $templateFilename = "log-bucket"
 $parametersFilename = "log-bucket"
 
-$templatePath = "$project/$component/templates/$templateFilename.yaml"
-$parameterPath = "$project/$component/parameters/$parametersFilename-$stage.json"
+$templatePath = Join-Path $PWD "$project/$component/templates/$templateFilename.yaml"
+if (! (Test-Path $templatePath)) {
+    throw 'Invalid template path or file not exist!'
+}
+$templateBody = Get-Content $templatePath -Raw
+
+$parameterPath = Join-Path $PWD "$project/$component/parameters/$parametersFilename-$stage.json"
+if (! (Test-Path $parameterPath)) {
+    throw 'Invalid parameter path or file not exist!'
+}
+$parameters = Get-Content $parameterPath | ConvertFrom-Json
+
+$tags = @(
+    @{ Key = 'Project'; Value = $project },
+    @{ Key = 'Component'; Value = $component },
+    @{ Key = 'Stage'; Value = $stage }
+)
+
+New-CFNStack `
+    -TemplateBody $templateBody `
+    -StackName $stackName `
+    -Parameter $parameters `
+    -Tag $tags
