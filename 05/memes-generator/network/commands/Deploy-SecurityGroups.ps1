751f5d9e65e63ff6b377d7e2ee27a542f6690e10
Jarosław Bagnicki <jarek@bagnicki.net>
2021-07-03 06:01:26 +0200

    Deploy SecurityGroup stack

---
+++ 05/memes-generator/network/commands/Deploy-SecurityGroups.ps1
@@ -0,0 +1,60 @@
+Import-Module AWS.Tools.Common
+Import-Module AWS.Tools.CloudFormation
+
+Set-DefaultAWSRegion 'eu-central-1'
+
+$stage = 'dev'
+$project = 'memes-generator'
+$component = 'network'
+
+$stack = 'security-groups'
+$stackName = "$project-$component-$stack-$stage"
+
+$templateFilename = 'security-groups'
+$parametersFilename = 'security-groups'
+
+$templatePath = Join-Path $PWD "$project/$component/templates/$templateFilename.yaml"
+if (! (Test-Path $templatePath)) {
+    Write-Error 'File with template not found!' -ErrorAction Stop
+}
+
+Write-Host "Loading template from: $templatePath ..." -ForegroundColor Blue
+$templateBody = Get-Content $templatePath -Raw
+
+$parameterPath = Join-Path $PWD "$project/$component/parameters/$parametersFilename-$stage.json"
+if (! (Test-Path $parameterPath)) {
+    Write-Error 'File with parameters not found!' -ErrorAction Stop
+}
+
+Write-Host "Loading parameterss from: $parameterPath ..." -ForegroundColor Blue
+$parameters = Get-Content $parameterPath | ConvertFrom-Json
+
+$tags = @(
+    @{
+        Key = 'Project'
+        Value = $project
+    },
+    @{
+        Key = 'Component'
+        Value = $component
+    },
+    @{
+        Key = 'Stage'
+        Value = $stage
+    }
+)
+
+Write-Host "Creating [$stackName] stack ..." -ForegroundColor Blue
+
+try {
+    New-CFNStack `
+        -StackName $stackName `
+        -TemplateBody $templateBody `
+        -Parameter $parameters `
+        -Tag $tags
+
+} catch {
+    Write-Error $_.Exception.Message -ErrorAction Stop
+}
+
+Write-Host "Stack created success." -ForegroundColor Green
