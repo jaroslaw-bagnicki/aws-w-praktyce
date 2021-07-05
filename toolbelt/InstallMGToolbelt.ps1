973a71071fa71dc1f731a91884978ff65d964a01
Jarosław Bagnicki <jarek@bagnicki.net>
2021-07-05 06:20:25 +0200

    Add installation script

---
+++ toolbelt/InstallMGToolbelt.ps1
@@ -0,0 +1,14 @@
+$moduleName = 'MemGenToolbelt'
+
+$delimeter = $IsWindows ? ';' : ':'
+$userModulesPath = $env:PSModulePath.Split($delimeter) | Where-Object { $_ -like "$HOME*" } | Select-Object -First 1
+
+$memGenModulePath = Join-Path $userModulesPath $moduleName 
+if (Test-Path $memGenModulePath) {
+    Write-Warning "$memGenModulePath folder exits!"
+    Remove-Item $memGenModulePath -Recurse
+    Write-Host
+}
+
+Copy-Item -Path ./MemGenToolbelt -Destination $userModulesPath -Recurse
+Write-Host "$moduleName installation success!" -ForegroundColor Green
